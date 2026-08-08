03d9a8e01c2bace9403e22af895e3a98a3dd1916
gglennd <grdimaano18@gmail.com>
2026-08-08 21:04:44 +0800

    feat: add base ui shadcn components

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 
+import { Button } from "@/components/ui/button";
+
 export function App() {
   return (
     <React.Fragment>
       <h1>Hello World!</h1>
+      <Button>Click me!</Button>
     </React.Fragment>
   );
 }
