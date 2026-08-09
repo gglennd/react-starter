6b8e7b757ce4638e1ef5769f53757bf498e55362
gglennd <grdimaano18@gmail.com>
2026-08-09 09:25:01 +0800

    feat: add seo head meta and favicon

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,12 +4,17 @@ import * as React from "react";
 import type { RouterContext } from "@/router";
 
 import appCss from "@/styles/global.css?url";
+import { seo } from "@/utils/seo";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
   head: () => ({
     meta: [
       { charSet: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
+      ...seo({
+        title: "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
+        description: "TanStack Start is a type-safe, client-first, full-stack React framework.",
+      }),
     ],
     links: [
       { rel: "stylesheet", href: appCss },
